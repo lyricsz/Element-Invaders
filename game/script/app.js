b7205190eec51ee0993eba4a30bff1c95b1a3980
lyricsz <152181901+lyricsz@users.noreply.github.com>
2024-09-01 02:54:27 +0100

    Add files via upload

--- game/script/app.js
+++ game/script/app.js
@@ -3,8 +3,21 @@ window.onload = () => {
 }
 
 const images = document.querySelectorAll("img");
+const log = document.getElementById("log");
+const enemy = document.getElementById("enemy");
+const currentScore = document.getElementById("currentScore");
+const highScore = document.getElementById("highScore");
+const earth = document.getElementById("earth");
+const air = document.getElementById("air");
+const water = document.getElementById("water");
+const fire = document.getElementById("fire");
 
 function start(){
+    const theme = document.getElementById("bg");
+    theme.volume = 0.5;
+    theme.play();
+    theme.loop = true;
+    theme.currentTime = 0;
     document.getElementById("replay").onclick = play;
     document.getElementById("gameOver").style.display = "none";
 
@@ -93,7 +106,7 @@ function start(){
             if(keypressed.includes("ArrowUp")){
                 if(this.gravity >= 0) {
                     this.gravity = 0;
-                    this.updateGravity = -8;
+                    this.updateGravity = -6;
                 }
             }
 
@@ -109,7 +122,12 @@ function start(){
             }
 
             if(keypressed.includes("Space")){
+                if(ui.bullet){
+                log.currentTime = 0;
+                log.volume = 0.2;
+                log.play();
                 this.fire()
+                }
             }
 
             lasers.forEach(laser => {
@@ -127,14 +145,14 @@ function start(){
         }
         fire(){
             ui.bullet--;
-            game.gameObjects.push(new Laser(this.x + this.width, this.y + this.height * 0.8, 100, 4));
+            game.gameObjects.push(new Laser(this.x + this.width, this.y + this.height * 0.8, 200, 4));
             lasers.push(game.gameObjects[game.gameObjects.length - 1]);
         }
     }
 
     class Element extends GameObject{
         constructor(x, y, width, height){
-            super(2, 0.1, 3, 0 + 52, -100, gameWidth  , gameHeight - height - 26);
+            super(2, 0.1, 2, 0 + 52, -100, gameWidth, gameHeight - height - 26);
             this.x = x;
             this.y = y;
             this.width = width;
@@ -149,6 +167,8 @@ function start(){
             this.frameX = Math.floor(Math.random() * 4);
             this.element = ["earth", "water", "fire", "air"];
             this.sElemet = this.element[this.frameX];
+            this.fireWaitTime = 50;
+            this.fireTime = 0;
         }
         update(deltaTime){
             const deltaScale = deltaTime / 100;
@@ -163,18 +183,22 @@ function start(){
                 this.alreadyStart = true;
             }
             if(this.start){
-                this.x -= 0.8 * deltaScale;
+                this.x += 0.8 * deltaScale;
             }
             
             this.angle += 0.4;
             this.time += 3 * (deltaScale);
             if(this.time > this.waitTime) {
                 this.start = true;
-                this.fire();
                 if(this.waitTime > 60) this.waitTime -= 20;
                 this.time = 0;
             }
-
+            if(this.fireTime > this.fireWaitTime){
+                this.fire();
+                this.fireTime = 0;
+            } else {
+                this.fireTime += deltaScale;
+            }
             lasers.forEach(laser => {
                 if(checkCollision(laser, this)){
                     if(this.gravity < 0) return;
@@ -182,21 +206,29 @@ function start(){
                     this.y = laser.y - this.height;
                     this.gravity =   0;
                     this.updateGravity = 0;
-                    if(this.x < laser.x + 10) this.jump = true;
+                    for(let i = 0; i < 5; i++){
+                        // if(randomVec4().r < 128){
+                        //     game.gameObjects.push(new Particle(this.x + this.width * 0.5, this.y + this.height, Math.random() * 2 + 2, Math.floor(Math.random() + 2) + 1, "rectangle"));
+                        // } else {
+                            game.gameObjects.push(new Particle(this.x + this.width * 0.5, this.y + this.height, Math.random() * 2 + 2, Math.floor(Math.random() * 2) + 1, "circle"));
+                        //}
+                    }
+                    if(this.x < laser.x + 10) this.jump = true, enemy.currentTime = 0, enemy.play(); 
                 } else {
                     this.updateGravity = 0.5;
                 }
             }); 
             if(this.jump){ 
                 if(this.gravity >= 0) {
-                    this.gravity = -20; 
-                    this.updateGravity = -1;
+                    this.gravity = -10; 
+                    this.updateGravity = -15;
                 }
                 this.jump = false;
             }
             if(this.y == this.maxY2){
-                if(this.x < 80) {
+                if(this.x + this.width > gameWidth * 0.8) {
                     ui.score+=200;
+                    ui.health = ui.maxHealth;
                     if(this.sElemet === "fire"){
                         elementSaved.fire++;
                     } else if (this.sElemet === "air"){
@@ -207,16 +239,15 @@ function start(){
                         elementSaved.earth++;
                     }
                 }
-                else ui.stability -= 50;
+                else ui.stability -= 50, ui.health -= 20;
                 this.markedForDeletion = true;
             };
-            super.update(deltaTime);    
+            super.update(deltaTime);
         }
         draw(){
             context.drawImage(this.image[0],this.frameX * this.width, 0, this.width, this.height, this.x, this.y, this.width, this.height); 
         }
         fire(){
-            ui.bullet--;
             game.gameObjects.push(new Laser(this.x - 5, this.y + this.height * 0.5, 10, 4, true));
             lasers.push(game.gameObjects[game.gameObjects.length - 1]);
         }
@@ -232,14 +263,16 @@ function start(){
             this.flipDir = flipDir || false;
             this.markedForDeletion = false;
             this.img = images[4];
+            this.velocityX = Math.floor(Math.random() * 4) + 4;
         }
         update(deltaTime){
             const deltaScale = deltaTime / 100;
             if(!this.flipDir){
-                this.x+= Math.floor(Math.random() * 4) + 4 * deltaScale;
+                this.x+= this.velocityX * deltaScale;
             } else {
-                this.x-= Math.floor(Math.random() * 4) + 4 * deltaScale;
+                this.x-=this.velocityX * deltaScale;
             }
+            this.velocityX+=0.1;
         }
         draw(){
             context.drawImage(this.img, this.x, this.y, this.width, this.height);
@@ -272,6 +305,8 @@ function start(){
             this.slow = 0;
             this.slowTime = 5;
             this.score = 0;
+            this.bulletTime = 0;
+            this.bulletAdd = 5000;
         }
         update(deltaTime){
             if(this.health < 0 || this.stability < 0) gameOver();
@@ -287,7 +322,12 @@ function start(){
             } else {
                 this.slow+=deltaTime;
             }
-          
+            if(this.bulletTime > this.bulletAdd){
+                this.bullet+=3;
+                this.bulletTime = 0;
+                if(this.bulletAdd > 2000)this.bulletAdd-=50;
+            } else this.bulletTime+=deltaTime;
+            if(this.bullet > this.maxBullet) this.bullet = this.maxBullet;
         }
         draw(){
             context.fillStyle = "white"
@@ -308,15 +348,15 @@ function start(){
             context.fillRect(0, 0, gameWidth, 10);
             context.closePath();
             context.fillStyle = "wheat";
-            context.font = "22px Comic Sans MS Bold";
+            context.font = "24px Comic Sans MS Bold";
             context.textAlign = "left";
             context.textBaseline = "middle";
             context.fillText("Stability: " + this.stability, 20, 40);
             context.font = "30px Comic Sans MS"
-            context.fillText("Score: " + this.score, gameWidth * 0.8, gameHeight * 0.1);
+            context.fillText("Score: " + this.score, gameWidth * 0.7, gameHeight * 0.1);
             context.fillStyle = "gray"
-            context.fillText("Score: " + this.score, gameWidth * 0.8 + 2, gameHeight * 0.1 + 2);
-            context.font = "22px Comic Sans MS Bold";
+            context.fillText("Score: " + this.score, gameWidth * 0.7 + 2, gameHeight * 0.1 + 2);
+            context.font = "24px Comic Sans MS Bold";
             context.fillText("Stability: " + this.stability, 20 + 1, 40+1);
             for(let i = 0; i < this.maxBullet; i++){
                 context.fillStyle = "gray"; 
@@ -329,22 +369,57 @@ function start(){
                 context.fillStyle = "white"; 
                 context.fillRect(20 + i * 4, 56, 2, 15);
             }
+            context.fillStyle = "tomato";
+            context.fillRect(gameWidth *  0.8, gameHeight - 26, gameWidth * 0.2, 26);
         }
     }
 
-    const input = new Input();
-    const player = new Player(-10, 10, 32, 32);
-    const element = new Element( gameWidth * 0.5, -10, 32, 32);
-    const ui = new UI(100, 100, 500, 40, 0);
-    const game = new Game(0, [input, player, element, ui]);
-    console.log(game.gameObjects)
+    class Particle{
+        constructor(x, y, radius, scale, type){
+            this.x = x;
+            this.y = y;
+            this.scale = scale;
+            this.radius = radius;
+            this.type = type;
+            this.markedForDeletion = false;
+            this.vx = Math.random() * (1 + 0.5) + -  0.5;
+            this.vy = Math.random() * (1 + 0.5) + -  0.5;
+            this.color = Math.floor(Math.random() * 360);
+            this.opacity = 1;
+        }
+        update(){
+            this.x+=this.vx;
+            this.y+=this.vy;
+            this.opacity -= 0.002;
+            if(this.scale > 0.1) this.scale-=0.01
+            if(this.opacity <= 0) this.markedForDeletion = true;
+        }
+        draw(){
+            context.fillStyle = `hsla(${this.color}, 50%, 50%, ${this.opacity})`;
+            context.strokeStyle = "rgba(0, 0, 0, 0)";
+            if(this.type == "circle"){
+                context.beginPath();
+                context.arc(this.x, this.y, this.radius * this.scale, 0, Math.PI * 2);
+                context.fill();
+                context.closePath();
+            } else if(this.type == "rectangle"){
+                context.fillRect(this.x, this.y, this.radius * this.scale, this.radius * this.scale);
+            }
+        }
+    }
 
+    let input = new Input();
+    let player = new Player(-100, 10, 32, 32);
+    let element = new Element( 20, -50, 32, 32);
+    let ui = new UI(100, 100, 500, 40, 0);
+    let game = new Game(0, [input, element, ui, player]);
+    
     let animateR;
 
     let check = 0;
-    let maxCheck = 400;
-    lastTime = 0;
-    function animate(time){
+    let maxCheck = 300;
+
+    function animate(time = 0){
         const deltaTime = time - lastTime;
         lastTime = time;
         game.update(deltaTime);
@@ -354,14 +429,10 @@ function start(){
         game.filter();
         input.draw();
         if(check > maxCheck) {
-            if(check > 600){
-                check = 0;
-            }
-            
-            if(check !== 0) {
+            if(check > maxCheck + 40) {
                 check = 0;
                 if(maxCheck > 40) maxCheck -= 20;
-                game.gameObjects.push(new Element( gameWidth * 0.5, -10, 32, 32));
+                game.gameObjects.push(new Element(20, -50, 32, 32));
             }
         }
         animateR = requestAnimationFrame(animate);
@@ -378,10 +449,48 @@ function start(){
     animate(0);
 
     function gameOver(){
+        if(window.localStorage.getItem("elementsInvadersHighscore") < ui.score){
+            window.localStorage.setItem("elementsInvadersHighscore", ui.score);
+        }
+        currentScore.textContent = ui.score;
+        highScore.textContent = window.localStorage.getItem("elementsInvadersHighscore");
+        earth.textContent = elementSaved.earth;
+        fire.textContent = elementSaved.fire;
+        water.textContent = elementSaved.water;
+        air.textContent = elementSaved.air;
         document.getElementById("gameOver").style.display = "block";
     }
-}
 
-function play(){
-    start();
+    function play(){
+        theme.volume = 0.5;
+        theme.play();
+        theme.loop = true;
+        theme.currentTime = 0;
+        document.getElementById("replay").onclick = play;
+        document.getElementById("gameOver").style.display = "none";
+    
+        keypressed = [];
+        elementSaved.air, elementSaved.earth, elementSaved.fire, elementSaved.water = 0;
+    
+        lastTime = 0;
+        lasers = [];
+        input = new Input();
+        player = new Player(-100, 10, 32, 32);
+        element = new Element( gameWidth * 0.5, -10, 32, 32);
+        ui = new UI(100, 100, 500, 40, 0);
+        game = new Game(0, [input, element, ui, player]);
+        check = 0;
+        maxCheck = 300;
+
+        animate(0);
+    }
+
+    function randomVec4(a){
+        return {
+            r: Math.floor(Math.random() * 256),
+            g: Math.floor(Math.random() * 256),
+            b: Math.floor(Math.random() * 256),
+            a: a
+        }
+    }
 }
